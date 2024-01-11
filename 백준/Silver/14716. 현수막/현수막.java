0b8d22d453d7d5d164b306655b28466e5c9c11be
westyunn <139761044+westyunn@users.noreply.github.com>
2024-01-11 23:56:24 +0900

    [Silver I] Title: 현수막, Time: 260 ms, Memory: 23880 KB -BaekjoonHub

---
+++ 백준/Silver/14716. 현수막/현수막.java
@@ -0,0 +1,65 @@
+import java.awt.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.StringTokenizer;
+
+public class Main {
+    static int N, M;
+    static int[] dr = { -1, 1, 0, 0, -1, -1, 1, 1};
+    static int[] dc = { 0, 0, -1, 1, 1, -1, -1, 1};
+    static int[][] map;
+    static boolean[][] visited;
+    static int cnt;
+    static Point p = new Point();
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        map = new int[N][M];
+        visited = new boolean[N][M];
+
+        for(int i=0; i<N; i++){
+            st = new StringTokenizer(br.readLine());
+            for(int j=0; j<M; j++){
+                map[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+        for(int i=0; i<N; i++){
+            for(int j=0; j<M; j++){
+                if(map[i][j] == 1 && !visited[i][j]){
+                    BFS(i, j);
+                    cnt++;
+                }
+            }
+        }
+        System.out.println(cnt);
+
+    } // main
+
+    public static void BFS(int x, int y){
+        Queue<Point> queue = new LinkedList<>();
+        visited[x][y] = true;
+        queue.add(new Point(x, y));
+        while(!queue.isEmpty()){
+            Point now = queue.poll();
+            int nowr = now.x;
+            int nowc = now.y;
+            for(int d=0; d<8; d++){
+                int nr = nowr + dr[d];
+                int nc = nowc + dc[d];
+                if(nr >= 0 && nc >= 0 && nr < N && nc <M && !visited[nr][nc] && map[nr][nc] == 1){
+                    queue.add(new Point(nr, nc));
+                    visited[nr][nc] = true;
+                }
+            }
+        }
+    }
+
+
+}
