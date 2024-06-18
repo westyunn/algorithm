7c36fe491af2cb6c789cba332832112c5c43d243
이서윤 <139761044+westyunn@users.noreply.github.com>
2024-06-18 17:38:26 +0900

    [level 1] Title: K번째수, Time: 0.07 ms, Memory: 33.6 MB -BaekjoonHub

---
+++ 프로그래머스/1/42748. K번째수/K번째수.js
@@ -0,0 +1,18 @@
+function solution(array, commands) {
+    var answer = [];
+    var arr = [];
+    // i ~ j 까지 자르고 정렬 후 k번째 숫자를 뽑으면 된다.
+    for(let l = 0; l < commands.length; l++) {
+        arr = [];
+        let i = commands[l][0];
+        let j = commands[l][1];
+        let k = commands[l][2];
+        arr = array.slice(i-1, j);
+        // javascript의 sort() 함수는 문자열로 변환하여 정렬하기 때문에 유니코드 포인트 순서로 정렬됨 
+        // 숫자 배열로 정렬하려면 아래와 같이 정렬해야함 
+        arr.sort((a, b) => a - b);
+        let num = arr[k-1];
+        answer.push(num);
+    }
+    return answer;
+}
