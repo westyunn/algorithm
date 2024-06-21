9efaff2ea4a9664923c2049035735d2833da9515
이서윤 <139761044+westyunn@users.noreply.github.com>
2024-06-21 15:15:38 +0900

    [Silver III] Title: 가희와 키워드, Time: 2464 ms, Memory: 225952 KB -BaekjoonHub

--- 백준/Silver/22233. 가희와 키워드/가희와 키워드.java
+++ 백준/Silver/22233. 가희와 키워드/가희와 키워드.java
@@ -1,39 +1,28 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.StringTokenizer;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-        Map<String, Boolean> keyword = new HashMap<>();
+        // HashMap대신 HashSet
+        Set<String> keyword = new HashSet<>();
+        StringBuilder sb = new StringBuilder();
         int N = Integer.parseInt(st.nextToken()); // 키워드 개수
         int M = Integer.parseInt(st.nextToken()); // 블로그에 쓴 글 개수
         for(int i=0; i<N; i++) {
-            String str = br.readLine();
-            keyword.put(str, false);
+            keyword.add(br.readLine());
         }
-        int answer = N;
-        String[] notes = new String[0];
         for(int i=0; i<M; i++) {
-            String str = br.readLine();
-            if(str.contains(",")){
-                notes = str.split(",");
-            } else {
-                notes = new String[1];
-                notes[0] = str;
+            // 아예 stringTokenizer을 사용해 쉼표 기준으로 분리 
+            st = new StringTokenizer(br.readLine(),",");
+            while(st.hasMoreTokens()) {
+                String str = st.nextToken();
+                keyword.remove(str);
             }
-            for(String notesWord : notes) {
-                // 키워드에 포함되고, 아직 처리되지 않은 키워드일 경우
-                if(keyword.containsKey(notesWord) && !keyword.get(notesWord)) {
-                    keyword.put(notesWord, true);
-                    answer--;
-                }
-            }
-            System.out.println(answer);
+            System.out.println(keyword.size());
         }
     }
 }
