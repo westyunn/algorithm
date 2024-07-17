7c8411d0e7e1d363729989712aae8f82e51bb57c
이서윤 <139761044+westyunn@users.noreply.github.com>
2024-07-18 02:04:50 +0900

    [Silver III] Title: 어린 왕자, Time: 180 ms, Memory: 15592 KB -BaekjoonHub

---
+++ 백준/Silver/1004. 어린 왕자/어린 왕자.java
@@ -0,0 +1,74 @@
+import java.awt.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.rmi.UnexpectedException;
+import java.util.ArrayList;
+import java.util.StringTokenizer;
+
+class Planet {
+    int x;
+    int y;
+    int radius;
+    Planet(int x, int y, int radius) {
+        this.x = x;
+        this.y = y;
+        this.radius = radius;
+    }
+}
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        // 출발점(도착점)이 원 안에 있는지 체크 두 점이 같은 원 안에 있으면 해당X
+        int tc = Integer.parseInt(br.readLine());
+        for(int t=0; t<tc; t++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            ArrayList<Point[]> list = new ArrayList<>();
+            int stX = Integer.parseInt(st.nextToken());
+            int stY = Integer.parseInt(st.nextToken());
+            int endX = Integer.parseInt(st.nextToken());
+            int endY = Integer.parseInt(st.nextToken());
+            list.add(new Point[]{new Point(stX, stY), new Point(endX, endY)});
+            int n = Integer.parseInt(br.readLine()); // 행성 개수
+            ArrayList<Planet> planets = new ArrayList<>();
+            for(int i=0; i<n; i++) {
+                st = new StringTokenizer(br.readLine());
+                int x = Integer.parseInt(st.nextToken());
+                int y = Integer.parseInt(st.nextToken());
+                int r = Integer.parseInt(st.nextToken());
+                planets.add(new Planet(x, y, r));
+            }
+            // 점과 점 사이의 거리 계산해서 해당 원 반지름과 비교
+            int cnt = 0;
+            for(int i=0; i<planets.size(); i++) {
+                int stx = list.get(0)[0].x;
+                int sty = list.get(0)[0].y;
+                int endx = list.get(0)[1].x;
+                int endy = list.get(0)[1].y;
+
+                int x2 = planets.get(i).x;
+                int y2 = planets.get(i).y;
+                int r = planets.get(i).radius;
+
+                if(checkSt(stx, sty, x2, y2, r) && !checkEnd(endx, endy, x2, y2, r)) {
+                    cnt++;
+                } else if(!checkSt(stx, sty, x2, y2, r) && checkEnd(endx, endy, x2, y2, r)) {
+                    cnt++;
+                }
+            }
+            sb.append(cnt).append('\n');
+
+        }
+        System.out.println(sb);
+    }
+    public static boolean checkSt(int x1, int y1, int x2, int y2, int r) {
+        double dist = Math.sqrt(Math.pow(x1 - x2, 2) + Math.pow(y1 - y2, 2));
+        return dist<=r; // true면 범위 안 false면 범위 밖
+    }
+
+    public static boolean checkEnd(int x1, int y1, int x2, int y2, int r) {
+        double dist = Math.sqrt(Math.pow(x1 - x2, 2) + Math.pow(y1 - y2, 2));
+        return dist <= r;
+    }
+}
